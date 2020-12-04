7488974759f6198a797378518d129b20e5d2d9ad
ZHB <2498850401@qq.com>
2020-12-04 11:42:53 +0800

    加入创建数据库和表的各项配置.

--- app/src/main/java/com/windmillweather/android/gson/AQI.java
+++ app/src/main/java/com/windmillweather/android/gson/AQI.java
@@ -1,4 +1,9 @@
 package com.windmillweather.android.gson;
 
 public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
 }
--- app/src/main/java/com/windmillweather/android/util/HttpUtil.java
+++ app/src/main/java/com/windmillweather/android/util/HttpUtil.java
@@ -1,14 +1,12 @@
-package com.windmillweather.android;
-
-import javax.security.auth.callback.Callback;
+package com.windmillweather.android.util;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-class HttpUtil {
-    public static void sendOkHttpRequest(String address, Callback callback) {
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
         OkHttpClient client=new OkHttpClient();
         Request request=new Request.Builder().url(address).build();
-        client.newCall(request).enqueue((okhttp3.Callback) callback);
+        client.newCall(request).enqueue(callback);
     }
 }
